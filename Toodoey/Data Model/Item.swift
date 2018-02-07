e5f5630dd01e7b9868c8252579aad93af8eec9f3
Aman Jaiswal <amanjaiswal42@yahoo.com>
2018-02-07 13:17:10 +0100

    Convert to MVC design pattern

---
+++ Toodoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Toodoey
+//
+//  Created by Aman Jaiswal on 2018. 02. 05..
+//  Copyright © 2018. Aman Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
